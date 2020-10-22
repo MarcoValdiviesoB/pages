cab412315311e89a958458ce10a9ee416ec325d2
marco <marco.valdiviesob@gmai.com>
2020-10-21 22:12:43 -0300

    services & works v1

--- src/app/views/landing/components/services/services.component.ts
+++ src/app/views/landing/components/services/services.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesComponent implements OnInit {
 
+  services = [
+    {nombre:"Software" , img : "https://ionicframework.com/img/homepage/components-1x.png"},
+    {nombre:"Aplicacion Movil", img : "https://ionicframework.com/img/homepage/run-anywhere-1x.png"},
+    {nombre:"Pagina Web" , img:"https://ionicframework.com/img/homepage/tooling-1x.png"}
+
+  ]
+
   constructor() { }
 
   ngOnInit() {
--- src/app/views/landing/components/works1/works1.component.ts
+++ src/app/views/landing/components/works1/works1.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Works1Component implements OnInit {
 
-  constructor() { }
+  works = [
+    { src:"https://almut.cl" , texto: ""},
+    { src:"https://bosquenaturapucon.cl" , texto: ""},
+    { src:"https://carecaca.club" , texto: ""}
+
+  ]
+
+  constructor() {
+    document.addEventListener("wheel", function (e) {
+      const xPoint = e.clientX
+      const yPoint = e.clientY
+      const iframe = document.getElementById("wrapper")
+      const rect = iframe.getBoundingClientRect();
+
+
+      if(xPoint > rect.left && yPoint < rect.bottom && xPoint < rect.right && yPoint > rect.top){
+        console.log("scrolling", e.deltaY)
+      }
+    })
+  }
 
   ngOnInit() {
+  }
+
+  scrollIframe(event){
+    console.log(event, document.getElementById("content"))
   }
 
 }
